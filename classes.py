5a208d62b214f08791370a12468d52015a3de481
Thulasid1425 <149058047+Thulasid1425@users.noreply.github.com>
2023-10-31 07:35:38 -0700

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,25 @@
+#Class employee:
+class Employee:
+    firstName:str="Thulasi"
+    lastName:str="Tendu"
+    def fullName(self):
+        return self.firstName+" "+self.lastName
+emp = Employee()
+print(emp.fullName())
+
+#class fibseries
+
+def fibEx(self):
+    n=10
+    a=1
+    b=0
+    i=1
+    lst = []
+    while i<=n:
+        lst.append()
+        a=a+b
+        b=a-b
+        i+=1
+        print(lst)
+        fib=fibseries()
+        fib.fibEx()
